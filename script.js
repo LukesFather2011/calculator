bda06d51acf3a5e79b274ebf19fb5fc113972164
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-10-02 17:28:14 -0500

    make variables to hold display, create function to call calculations

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+
+// for the calculator display in html
+let firstNumber  = 0;
+let secondNumber = 0;
+let operator     = "";
+
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+
+        case "-":
+            subtract(num1, num2);
+            break;
+
+        case "*":
+            multiply(num1, num2);
+            break;
+
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+};
